164d45014a70f5911e6abbeff9e031d85b589d11
GabbyMoreno <gaby_sweet_16@hotmail.com>
2013-07-02 19:31:38 -0500

    clase coordinacion de desarrollo academico

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentopersonal/CoordinacionDesarrolloAcademico.java
@@ -0,0 +1,15 @@
+package edu.utvm.departamentofinanza.departamentopersonal;
+
+public class CoordinacionDesarrolloAcademico{
+  public String personalAcademico;
+	public String carreras;
+	
+	private void generarVacantes(){
+	
+	}
+	
+	private void evaluarExpedientesPersonales(){
+		
+	}
+	
+}
