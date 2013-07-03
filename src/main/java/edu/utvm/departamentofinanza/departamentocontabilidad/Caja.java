6af4658bff4f8e4e63007007e46e7603e5de2b2a
SOrtMart <oms_alfda92@hotmail.com>
2013-07-03 14:24:14 -0500

    Create Caja.java

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentocontabilidad/Caja.java
@@ -0,0 +1,16 @@
+package edu.utvm.departamentofinanza.departamentocontabilidad;
+import package edu.utvm.subdireccionplaneacion.serviciosescolares.Alumno;
+
+public class Caja {
+  public ReciboPago reciboPago;
+	public Float dinero;
+	public Alumno alumno; 
+
+	public caja  (){
+		this.reciboPago = new ReciboPago();
+	}
+
+	public alumno(String){
+		
+	}
+}
