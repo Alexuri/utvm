c4d41c320e5156704987f637ef7e01fbf3961e14
Alma Edith Tepetate Romero <alma7@UL-TRON.(none)>
2013-07-03 17:56:03 -0500

    primerra actualizacion

---
+++ src/main/java/edu/utvm/desarrolloacademico/coordinacioncontinua/Formatos.java
@@ -0,0 +1,15 @@
+package edu.utvm.DireccionEducacionContinua;
+import java.util.*;
+public class Formatos{
+	private String tipo;
+	private date fecha;
+	private int folio;
+	private String vistoBueno;
+
+	public Formatos( String tipo, date fecha, int folio,String vistoBueno){
+		this.tipo = tipo;
+		this.fecha = fecha;
+		this.folio = folio;
+		this.vistoBueno = vistoBueno;
+	}
+}
