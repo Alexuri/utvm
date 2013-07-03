935f2bdfa73496ecc468235a56d1fdfb061fd5c4
Xaty <xhuxhys14@gmail.com>
2013-07-03 18:55:33 -0500

    soporte

---
+++ src/main/java/edu/utvm/direccioncarrera/carrera/Soporte.java
@@ -0,0 +1,13 @@
+package edu.utvm.direccioncarrera.departamento;
+import edu.utvm.*;
+
+public class SoporteTecnico(){
+  
+	public JefeArea jefeArea;
+
+		public SoporteTecnico(){
+		
+			super.jefeArea = new JefeArea;
+		}
+	}
+}
