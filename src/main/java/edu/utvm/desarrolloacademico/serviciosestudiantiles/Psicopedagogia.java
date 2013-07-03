f1a8df60262ce629bc201d06459f7a09e9e2588f
093359 <ismaelqh131@gmail.com>
2013-07-03 14:54:43 -0500

    Psicopedagogia
    Psicopedagogia

---
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/Psicopedagogia.java
@@ -0,0 +1,40 @@
+public class Psicopedagogia
+{
+  public ArrayList<Psicologo> psicologas;
+	
+	public psicopedagogia()
+	{
+		psicologas = new ArrayList<Psicologo>();
+	}
+
+		public void tratamientoTipoBeca()
+		{
+
+
+		}
+		
+		public void canalizacionParaEntrevistas()
+		{
+
+
+		}
+
+		public void cambioCarrera()
+		{
+
+
+		}
+
+		public void solicitudTipoBeca()
+		{
+
+
+		}
+
+		public void visitaDomiciliaria()
+		{
+
+
+		}
+}
+
