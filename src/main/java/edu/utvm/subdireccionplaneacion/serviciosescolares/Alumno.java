64242f5c2c809f9977045678d318cb6259f4237d
josepzuniga <josem.88@hotmail.com>
2013-07-03 16:47:12 -0500

    Alumno.java creado
    clase alumno creada

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/serviciosescolares/Alumno.java
@@ -0,0 +1,117 @@
+Package edu.utvm.subdireccionplaneacion.serviciosescolares
+import.java.until.*
+
+public class Alumno{
+
+  public String matricula;
+	public Persona persona;
+
+	public Alumno(String matricula){
+		
+		Persona persona = new Persona();
+	}
+
+	public void otorgaDoc(){
+
+	}
+	
+	public void pagaServicio(){
+
+	}
+
+	public void solicitaAdeudos(){
+
+	}
+
+	public void inscribirse(){
+
+	}
+
+	public void solicitarDocumentos(){
+
+	}
+
+	public void SolicitarConstancia(){
+
+	}
+
+	public void SolicitarCredencial(){
+
+	}
+
+	public void EntregarDocTitulacion(){
+
+	}
+
+	public void SeRegstraEnActividades(){
+
+	}
+
+	public void ParticipaEnActividades(){
+
+	}
+
+	public void DesarrollaProyecto(){
+
+	}
+
+	public void RegistraProyecto(){
+
+	}
+
+	public void RealizaRegistro(){
+
+	}
+
+	public void TomaClases(){
+
+	}
+
+	public void ConsultaCalificaciones(){
+
+	}
+
+	public void RealizaTarea(){
+
+	}
+
+	public void RealizaEvaluaciones(){
+
+	}
+
+	public void RealizaAvtividades(){
+
+	}
+
+	public void EstudiaCarrera(){
+
+	}
+
+	public void Aprueba(){
+
+	}
+
+	public void Reprueba(){
+
+	}
+
+	public void EvaluaDocentes(){
+
+	}
+
+	public void EvaluaServiciosComplementarios(){
+
+	}
+
+	public void ConsultaInfo(){
+
+	}
+
+	public void SolicitaLibro(){
+
+	}
+
+	public void EntregaLibro(){
+
+	}
+}
