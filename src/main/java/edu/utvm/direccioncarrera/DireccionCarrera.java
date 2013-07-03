6b909340c7478630d281181730046241fadea827
Xaty <xhuxhys14@gmail.com>
2013-07-03 18:24:20 -0500

    2do intento

---
+++ src/main/java/edu/utvm/direccioncarrera/DireccionCarrera.java
@@ -0,0 +1,15 @@
+package edu.utvm.direccioncarrera;
+import edu.utvm.*;
+
+public class DireccionCarrera(){
+  public Carrera carrera;
+	public JefeArea jefeArea;
+
+		public  DireccionCarrera(){
+		
+			this.jefeArea = new JefeArea;
+			this.carrera = new Carrera;
+		
+		}
+	
+}
