ccb2d14c41890aac64afd3170f24e4c710abeb05
Jovanywater <jovany_778@hotmail.com>
2013-07-03 14:50:55 -0500

    creando clase RegistroImss

---
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosmedicos/RegistroImss.java
@@ -0,0 +1,23 @@
+publiic class RegistroImss
+{
+  private Alumno alumno;
+	Private Empleado empleado;
+	String numeroAfiliacion;
+        
+	public RegistroImss(Alumno alumno,String numeroAf)
+	{
+         this.alumno = new Alumno();
+         this.numeroAf=new numeroAfiliacion;();
+		
+	}
+
+	public RegistroImss(Empleado empleado,String numeroAf)
+	{
+         this.empleado = new Empleado();
+         this.numeroAf=new numeroAfiliacion;();
+		
+	}
+
+
+
+}
