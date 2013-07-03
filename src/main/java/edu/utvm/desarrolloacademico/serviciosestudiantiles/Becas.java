0ab889254ba660c189f3345a1f6a981a46e9fe1c
093359 <ismaelqh131@gmail.com>
2013-07-03 14:53:36 -0500

    Becas
    Becas

---
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/Becas.java
@@ -0,0 +1,52 @@
+public class Becas
+{
+
+String idBeca;
+String descripcion;
+double montoBeca;
+
+
+
+public ArrayList<Psicologo> psicologas;
+  
+	public becas()
+	{
+		psicologas = new ArrayList<Psicologo>();
+	}
+
+		public void recepcionSolicitud()
+		{
+
+
+		}
+
+		public void envioSolicitud()
+		{
+
+
+		}
+
+		public void informarResultadoAlumno()
+		{
+
+
+		}
+
+		public void verificarPromedio()
+		{
+
+
+		}
+
+		public void notificarBajas()
+		{
+
+
+		}
+
+		public void renovacionBecas()
+		{
+
+
+		}
+}
