b5a9e486f46bbd073912dd1dadc2456f9add0bd7
GabbyMoreno <gaby_sweet_16@hotmail.com>
2013-07-02 19:27:18 -0500

    clase contratacion

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentopersonal/Contratacion.java
@@ -0,0 +1,15 @@
+package edu.utvm.departamentofinanza.departamentopersonal;
+
+public class Contratacion{
+  public CoordinacionDesarrolloAcademico coordinacionDesarrolloacademico;
+	private Contrato contrato;
+	
+	public Contratacion(){
+		this.coordinacionDesarrolloacademico=new CoordinacionDesarrolloAcademico;
+		this.contrato = new Contrato;
+	
+	public void generarContrato(){
+		
+	}
+	
+}
