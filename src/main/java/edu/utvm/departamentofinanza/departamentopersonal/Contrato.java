270ec077002529405478b7c20bc09d5ba903e361
GabbyMoreno <gaby_sweet_16@hotmail.com>
2013-07-02 19:29:22 -0500

    clase contrato

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentopersonal/Contrato.java
@@ -0,0 +1,18 @@
+package edu.utvm.departamentofinanza.departamentopersonal;
+import package edu.utvm.Persona;
+import java.util.*;
+
+public class Contrato{
+  public Persona persona;
+	private String clausulas;
+	private Date fechaInicio;
+	private Date fechaTermino;
+	
+	public Contrato(Persona per, String clau, Date fechaIni, Date fechaFin){
+		this.persona = per;
+		this.clausulas = clau;
+		this.fechaInicio = fechaIni;
+		this.fechaTermino = fechaFin;
+	}
+	
+}
