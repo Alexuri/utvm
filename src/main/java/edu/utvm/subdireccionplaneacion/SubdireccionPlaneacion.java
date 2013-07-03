c2a0fe00f7ae5154c3da223177d1bc8c082caaf5
Jorge Perez Vixtha <jorgepv@UL-TRON.(none)>
2013-07-03 16:51:15 -0500

    clase SubdireccionPlaneacion

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
@@ -0,0 +1,9 @@
+package edu.utvm;
+import import edu.utvm.departamentofinanza.deparmentopersonal;
+class SubdireccionPlaneacion{
+	public ProgramacionPresuspuesto programacionpresuspuesto;
+	public ServiciosEscolares serviciosescolares;
+	public PracticasEstadias practicasestadias;
+	public JefeArea jefearea;
+	
+}
