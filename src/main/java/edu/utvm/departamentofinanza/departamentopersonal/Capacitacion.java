37590ce37c5b32937298112723f0eef1024ac49e
GabbyMoreno <gaby_sweet_16@hotmail.com>
2013-07-02 19:25:30 -0500

    clase capacitacion

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentopersonal/Capacitacion.java
@@ -0,0 +1,16 @@
+package edu.utvm.departamentofinanza.departamentopersonal;
+
+public class Capacitacion{
+  public String personalAcademico;
+	public String rectoria;
+	public String carreras;
+	
+	public void capacitarPersonal(){
+	
+	}
+	
+	public void proporcionarMaterialParaCapacitacion(){
+		
+	}
+	
+}
