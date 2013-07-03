494f1210e104643f4a36d75e830c842db16dbb36
093359 <ismaelqh131@gmail.com>
2013-07-03 14:55:44 -0500

    Requisitos
    Requisitos

---
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/Requisitos.java
@@ -0,0 +1,11 @@
+public class Requisitos
+{
+
+  Bolean actaNacimiento;
+	String curp;
+	Double promedio;
+	String situacionEconomica;
+	Bolean comprobanteDomicilio;
+	Bolean historialCalificaciones;
+
+}
