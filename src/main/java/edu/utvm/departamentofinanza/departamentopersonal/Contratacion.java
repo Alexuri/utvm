00b67b74e060d13045711192bd293ea0f05184ea
GabbyMoreno <gaby_sweet_16@hotmail.com>
2013-07-03 12:32:54 -0500

    ()

--- src/main/java/edu/utvm/departamentofinanza/departamentopersonal/Contratacion.java
+++ src/main/java/edu/utvm/departamentofinanza/departamentopersonal/Contratacion.java
@@ -5,8 +5,8 @@ public class Contratacion{
 	private Contrato contrato;
 	
 	public Contratacion(){
-		this.coordinacionDesarrolloacademico=new CoordinacionDesarrolloAcademico;
-		this.contrato = new Contrato;
+		this.coordinacionDesarrolloacademico=new CoordinacionDesarrolloAcademico();
+		this.contrato = new Contrato(Persona per, String clau, Date fechaIni, Date fechaFin);
 	}
 	
 	public void generarContrato(){
