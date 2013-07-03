9d27af76c0092ee705c7c7f5f89be0e6379b7a0d
Xaty <xhuxhys14@gmail.com>
2013-07-03 18:47:03 -0500

    carrera

---
+++ src/main/java/edu/utvm/direccioncarrera/carrera/Carrera.java
@@ -0,0 +1,27 @@
+package edu.utvm.direccioncarrera;
+import edu.utvm.*;
+
+public class  Carrera(){
+
+  public string planEstudio;
+	public string horario:
+	public int grado;
+	public string grupo;
+	public string ubicacion;
+	public string NombreCarrera;
+	
+	public Alumno alumno;
+	public Secretaria secretaria;
+	public Docente docente;
+	public Laboratorio
+		
+		public Carrera (){
+			this.alumno = new Alumno;
+			this.secretaria = new Secretaria;
+			this.docente = new Docente;
+			this.laboratorio = new Laboratorio;
+				
+		}
+}
+
+
