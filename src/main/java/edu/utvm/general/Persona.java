2be7b025a2bfe4ac7062b4efc488eeefbf9b6d26
Luis Enrique Angeles Mendieta <kikeangeles@UL-TRON.(none)>
2013-07-04 16:05:05 -0500

    Se movio la clase persona al paquete general

---
+++ src/main/java/edu/utvm/general/Persona.java
@@ -0,0 +1,30 @@
+package edu.utvm.general;
+import java.util.*;
+
+/*
+ * Esto es un comentario
+ * */
+
+public class Persona {
+  	private String nombre;
+  	private String apellidoPaterno;
+  	private String apellidoMaterno;
+  	private Date fechaNacimiento;
+  	private String rfc;
+  	private String curp;
+  	private String direccion;
+	private String correo;
+	private String telefono;
+	
+	public Persona(String nom, String apPat, String apMat, Date fechaNac, String rfc, String curp, String direc, String correo, String tel){
+		this.nombre = nom;
+		this.apellidoPaterno = apPat;
+		this.apellidoMaterno = apMat;
+		this.fechaNacimiento = fechaNac;
+		this.rfc = rfc;
+		this.curp = curp;
+		this.direccion = direc;
+		this.correo = correo;
+		this.telefono = tel; 
+	}
+}
