7d37a5b310bc554e0af278a9a25d7b66ed0c9e17
trocheman <jescobedo@utvm.edu.mx>
2013-07-03 19:18:09 -0500

    Create PracticasEstadias.java
    creando practicas y estadias

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/PracticasEstadias.java
@@ -0,0 +1,33 @@
+package edu.utvm.subdireccionplaneacion.practicasestadias;
+import edu.utvm.departamentofinanza.departamentopersonal;
+
+public class PracticasEstadias{
+
+  public ProcedimientoEstadia procedimientoEstadia;
+	public VisitaIndustrial visitaIndustrial;
+	public JefeArea jefeArea;
+
+	public ProcedimientoEstadia(ProcedimientoEstadia procedimientoEstadia,VisitaIndustrial visitaIndustrial,JefeArea jefeArea){
+		ProcedimientoEstadia procedimientoEstadia = new ProcedimientoEstadia();
+		VisitaIndustrial visitaIndustrial = new VisitaIndustrial();
+		JefeArea jefeArea = new JefeArea();
+	}
+
+	
+	
+	public void GeneraCarta(){
+
+	}
+
+	public void GestionarVisita(){
+	}
+
+	public void GenerarMemorandumVisita(){
+	}
+
+	public void AltaEstadia(){
+	}
+
+	public void BajaEstadia(){
+	}
+}
