51322a4c8004d10286aff99c9130353fb825c9bf
trocheman <jescobedo@utvm.edu.mx>
2013-07-03 19:13:53 -0500

    Create Empresa.java
    creando empresa

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/Empresa.java
@@ -0,0 +1,9 @@
+package edu.utvm.subdireccionplaneacion.practicasestadias;
+
+public class Empresa(){
+
+  public String rfcEmpresa;
+	public String nombreEmpresa;
+	public String direccionEmpresa;
+	public int telefonoEmpresa;
+}
