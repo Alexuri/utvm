59507a1788f0774238144f098594872bdef2e0b3
trocheman <jescobedo@utvm.edu.mx>
2013-07-03 19:14:52 -0500

    Create Organizacion.java
    creando organizacion

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/Organizacion.java
@@ -0,0 +1,20 @@
+package edu.utvm.subdireccionplaneacion.practicasestadias;
+
+
+public class Organizacion{
+
+  public String rfc;
+	public String nombreOrganizacion;
+	public String direccionOrganizacion;
+	public String correoOrganizacion;
+	public CartaTerminacion cartaTerminacion;
+
+
+	public Organizacion(CartaTerminacion cartaterminacion){
+
+		CartaTerminacion cartaterminacion = new CartaTerminacion();
+	}
+
+	public void GeneraCarta(){
+	}
+}
