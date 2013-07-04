5ec8f75ebfda224c30ab2e0b71fc34445bd52ba6
Ambar Janeth Hernandez Gonzalez <ambarutvm@UL-TRON.(none)>
2013-07-04 12:05:44 -0500

    Aqui esta mi trabajo diana

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentorecursos/sistemaCompra.java
@@ -0,0 +1,11 @@
+package prueba;
+
+public class sistemaCompra {
+
+	public static void main(String[] args) {
+		Compra vistaCompra = new Compra(1, 1111, "laptop");
+		System.out.println(vistaCompra);
+
+	}
+
+}
