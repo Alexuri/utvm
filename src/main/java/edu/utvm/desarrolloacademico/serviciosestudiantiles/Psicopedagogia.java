15ee761e7c27471382d7a3b80883723c10ac9bf1
093359 <ismaelqh131@gmail.com>
2013-07-04 20:15:03 -0500

    Update Psicopedagogia.java

--- src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/Psicopedagogia.java
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/Psicopedagogia.java
@@ -1,43 +1,33 @@
 package edu.utvm.desarrolloacademico.serviciosestudiantiles;
+
 import java.util.ArrayList;
 
-public class Psicopedagogia
-{
-  public ArrayList<Psicologo> psicologas;
-	
-	public psicopedagogia()
-	{
-		psicologas = new ArrayList<Psicologo>();
+import edu.utvm.departamentofinanza.departamentopersonal.Psicologo;
+
+public class Psicopedagogia {
+	public ArrayList<Psicologo> psicologo;
+
+	public Psicopedagogia() {
+		psicologo = new ArrayList<Psicologo>();
 	}
 
-		public void tratamientoTipoBeca()
-		{
+	public void tratamientoTipoBeca() {
 
+	}
 
-		}
-		
-		public void canalizacionParaEntrevistas()
-		{
+	public void canalizacionParaEntrevistas() {
 
+	}
 
-		}
+	public void cambioCarrera() {
 
-		public void cambioCarrera()
-		{
+	}
 
+	public void solicitudTipoBeca() {
 
-		}
+	}
 
-		public void solicitudTipoBeca()
-		{
+	public void visitaDomiciliaria() {
 
-
-		}
-
-		public void visitaDomiciliaria()
-		{
-
-
-		}
+	}
 }
-
