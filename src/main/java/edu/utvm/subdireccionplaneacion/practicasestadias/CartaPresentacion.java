ad6a6a110c4e0096f9ca0e0781831bde9dad1b31
josepzuniga <josem.88@hotmail.com>
2013-07-05 12:48:40 -0500

    Create CartaPresentacion.java
    creacion de la clase CartaPresentacion

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/CartaPresentacion.java
@@ -0,0 +1,19 @@
+package edu.utvm.subdireccionplaneacion.practicasestadias;
+import edu.utvm.subdireccionplaneacion.practicasestadias;
+
+public class CartaPresentacion{
+
+  public Int matricula;
+	public Alumno alumno;
+	public String carrera;
+	public String organizacion;
+	public String cuatrimestre;
+	public String fechaInicio
+	public String fechaFin;
+
+	public CartaPresentacion(){
+		
+		Alumno alumno = new Alumno();
+	}
+
+}
