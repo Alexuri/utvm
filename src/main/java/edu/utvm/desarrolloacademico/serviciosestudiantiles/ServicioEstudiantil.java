92338a63a8ce77a246ca5e1d95ccb5fe59bcd7a4
093359 <ismaelqh131@gmail.com>
2013-07-04 20:33:22 -0500

    Update and rename ServiciosEstudiantiles.java to ServicioEstudiantil.java

---
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosestudiantiles/ServicioEstudiantil.java
@@ -0,0 +1,11 @@
+package edu.utvm.desarrolloacademico.serviciosestudiantiles;
+
+public class ServicioEstudiantil {
+	Psicopedagogia psicopedagogia;
+	Beca beca;
+
+	public ServicioEstudiantil(Psicopedagogia psicopedagogia, Beca beca) {
+		this.psicopedagogia = psicopedagogia;
+		this.beca = beca;
+	}
+}
