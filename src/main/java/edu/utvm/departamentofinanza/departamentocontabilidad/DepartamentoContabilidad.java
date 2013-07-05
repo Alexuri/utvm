37912c8089944315226d4fee1b648d604eefd184
Your Name <you@example.com>
2013-07-05 10:17:15 -0500

    Corrigiendo errores

--- src/main/java/edu/utvm/departamentofinanza/departamentocontabilidad/DepartamentoContabilidad.java
+++ src/main/java/edu/utvm/departamentofinanza/departamentocontabilidad/DepartamentoContabilidad.java
@@ -1,16 +1,21 @@
 package edu.utvm.departamentofinanza.departamentocontabilidad;
-import package edu.utvm.departamentofinanza.departamentopersonal.Asistente;
-import package edu.utvm.departamentofinanza.departamentopersonal.Jefearea;
+import package edu.utvm.departamentofinanza.departamentopersonal.*;
 
 public class DepartamentoContabilidad{
-  public JefeArea jefeArea;
+	
+	public JefeArea jefeArea;
+	
 	public Asistente asistente;
+	
 	public Caja caja;
 
 	public DepartamentoContabilidad(){
-		this.caja = new Caja();
-		this.Asistente = new asistente();
-		this.JefeArea = new jefeArea();
+		this.caja = caja();
+		this.asistente = asistente();
+		this.jefeArea = jefeArea();
 	}
-        
+	
+	public void realizacontroldepago(){
+	
+	}
 }
