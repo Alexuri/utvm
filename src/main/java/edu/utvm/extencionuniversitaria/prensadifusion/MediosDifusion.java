d5d4fc25e859bddffac2fae215f9ba85c346616c
Laurikis <lauri_azul11@hotmail.com>
2013-07-05 11:34:16 -0500

    Clase MediosDifusion

---
+++ src/main/java/edu/utvm/extencionuniversitaria/prensadifusion/MediosDifusion.java
@@ -0,0 +1,12 @@
+package edu.utvm.extencionuniversitaria.PrensaDifusion;
+
+
+  public class MediosDifusion{
+		public String radio;
+		public String television;
+		public String revista;
+		public String folletos;
+
+			public void darAConocer(){
+			}
+	}
