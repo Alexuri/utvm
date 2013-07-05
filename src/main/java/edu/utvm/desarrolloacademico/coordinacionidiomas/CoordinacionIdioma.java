07c83334e60eea5254c564ad73e96a45858edae4
Jaime Salathiel Pejay Bautista <sala0211@hotmail.com>
2013-07-04 20:36:47 -0500

    Agregando Null en JefeArea

--- src/main/java/edu/utvm/desarrolloacademico/coordinacionidiomas/CoordinacionIdioma.java
+++ src/main/java/edu/utvm/desarrolloacademico/coordinacionidiomas/CoordinacionIdioma.java
@@ -1,7 +1,7 @@
 package edu.utvm.desarrolloacademico.coordinacionidiomas;
 import java.util.ArrayList;
-import edu.utvm.departamentofinanza.departamentopersonal.*;
 
+import edu.utvm.departamentofinanza.departamentopersonal.*;
 public class CoordinacionIdioma {
 
   private JefeArea jefe;
@@ -9,7 +9,7 @@ public class CoordinacionIdioma {
 	private ArrayList<Asistente> asistente;
 
 	public CoordinacionIdioma(JefeArea jefe,Docente docente, Asistente asistente ) {
-		this.jefe = new JefeArea();
+		this.jefe = new JefeArea(null, null, null, null, null, null, null, null, null, null, null);
 		this.docente = new ArrayList<Docente>();
 		this.asistente= new ArrayList<Asistente>();
 	}
