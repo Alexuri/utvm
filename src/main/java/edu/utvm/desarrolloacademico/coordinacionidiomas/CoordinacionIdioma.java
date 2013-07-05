a38a6cd736319e66f42125620706b99eb02b5cfd
AdrianaJuarez <juarez.adriana@hotmail.com>
2013-07-04 20:35:52 -0500

    Agregando modificaciones

--- src/main/java/edu/utvm/desarrolloacademico/coordinacionidiomas/CoordinacionIdioma.java
+++ src/main/java/edu/utvm/desarrolloacademico/coordinacionidiomas/CoordinacionIdioma.java
@@ -1,7 +1,7 @@
 package edu.utvm.desarrolloacademico.coordinacionidiomas;
 import java.util.ArrayList;
-
 import edu.utvm.departamentofinanza.departamentopersonal.*;
+
 public class CoordinacionIdioma {
 
   private JefeArea jefe;
