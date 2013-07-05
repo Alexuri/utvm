34675d216eca47067aec2167054bc8a20b62b694
german2 <germancruz88@gmail.com>
2013-07-05 10:20:01 -0500

    se realizo la correccion de recibo pago

---
+++ src/main/java/edu/utvm/departamentofinanza/departamentocontabilidad/ReciboPago.java
@@ -0,0 +1,18 @@
+package edu.utvm.departamentofinanza.departamentocontabilidad;
+
+public class Caja {
+  public ReciboPago reciboPago;
+	public Float dinero;
+	public String alumno; 
+
+	public Caja(ReciboPago reciboPago,Float dinero,String alumno) {
+		this.reciboPago= reciboPago;
+		this.dinero= dinero;
+		this.alumno= alumno;
+	}
+		
+		
+			public void RecibeDinero(){
+		
+	}
+}	
