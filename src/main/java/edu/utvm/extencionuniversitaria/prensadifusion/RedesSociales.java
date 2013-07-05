3732aff6c25c11a5c45081eec3cebc9a5d460101
Laurikis <lauri_azul11@hotmail.com>
2013-07-05 11:35:59 -0500

    Clase RedesSociales

---
+++ src/main/java/edu/utvm/extencionuniversitaria/prensadifusion/RedesSociales.java
@@ -0,0 +1,10 @@
+package edu.utvm.extencionuniversitaria.PrensaDifusion;
+
+  public class RedesSociales{
+		public String nombreDeRed;
+		public String direccion;
+		public String tipo;
+
+			public void difunde(){
+			}
+	}             
