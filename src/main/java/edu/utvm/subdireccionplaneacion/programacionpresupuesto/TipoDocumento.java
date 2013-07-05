86154ad319d0b8ffb921a2dfa046a66f3af937e5
JorgePV <georgefpv30@gmail.com>
2013-07-05 09:55:58 -0500

    se corrigio las errores encontrados

--- src/main/java/edu/utvm/subdireccionplaneacion/programacionpresupuesto/TipoDocumento.java
+++ src/main/java/edu/utvm/subdireccionplaneacion/programacionpresupuesto/TipoDocumento.java
@@ -1,5 +1,5 @@
-package edu.utvm;
-class TipoDocumento{
+package edu.utvm.subdireccionplaneacion.programacionpresupuesto;
+public class TipoDocumento{
 	public int folio;
 	public String nombre;
 	public String fechaElaboracion;
