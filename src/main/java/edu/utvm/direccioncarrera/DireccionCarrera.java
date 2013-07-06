4f2c9f13fc54071d322efafb988a896dcaa3ee8d
Upastor <uripast_@hotmail.com>
2013-07-06 02:23:16 +0200

    correccion import

--- src/main/java/edu/utvm/direccioncarrera/DireccionCarrera.java
+++ src/main/java/edu/utvm/direccioncarrera/DireccionCarrera.java
@@ -1,15 +1,15 @@
 package edu.utvm.direccioncarrera;
-import edu.utvm.*;
+import edu.utvm.departamentofinanza.departamentopersonal.JefeArea;
 
 public class DireccionCarrera(){
-  public Carrera carrera;
+	
+	public Carrera carrera;
 	public JefeArea jefeArea;
 
 		public  DireccionCarrera(){
 		
 			this.jefeArea = new JefeArea;
 			this.carrera = new Carrera;
-		
-		}
+				}
 	
 }
