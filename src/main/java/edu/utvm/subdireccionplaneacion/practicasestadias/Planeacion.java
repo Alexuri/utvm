1313c1cad61abf2c56cd1bbdfdcb1fd40b0e629e
josepzuniga <josem.88@hotmail.com>
2013-07-06 14:51:09 -0500

    Clase Planeacion modificada
    Se agrego un "import" y creo un constructor para Carrera

--- src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/Planeacion.java
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/Planeacion.java
@@ -1,11 +1,17 @@
 package edu.utvm.subdireccionplaneacion.practicasestadias;
-public class Planeacion(){
+import edu.utvm.direccioncarrera.Carrera;
+
+public class Planeacion{
 
   public String folioPlaneacion;
-	public String carrera;
+	public Carrera carrera;
 	public String grado;
 	public String grupo;
 	public String empresa;
 	public int asistentes;
 	public String tutorAsignado;
+	
+	public Planeacion{
+		Carrera carrera = new Carrera();
+	}
 }
