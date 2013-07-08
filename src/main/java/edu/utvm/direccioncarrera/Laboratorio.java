c3f5fb944a0aa9107ca98e41ca055ad00f06a8a9
unknown <pvisual2@pv2_22.utvm2008.mx>
2013-07-08 17:42:08 -0500

    Modificando las claces Laboratorio y DireccionCarrera

--- src/main/java/edu/utvm/direccioncarrera/Laboratorio.java
+++ src/main/java/edu/utvm/direccioncarrera/Laboratorio.java
@@ -1,16 +1,20 @@
 package edu.utvm.direccioncarrera;
+
 import edu.utvm.departamentofinanza.departamentopersonal.ResponsableLaboratorio;
 
-public class Laboratorio{
+public class Laboratorio {
 
-  	public String nombre;
+	public String nombre;
 	public String ubicacion;
 	public String equipos;
-	public String responsableLaboratorio;
-	
-		public Laboratorio(){
-		
-			this.responsableLaboratorio = new ResponsableLaboratorio();
-		
-		}
-} 
+	public ResponsableLaboratorio responsableLaboratorio;
+
+	public Laboratorio() {
+
+		this.responsableLaboratorio = new ResponsableLaboratorio("Able",
+				"Gonzalez", "Luna", null, "ABL3423", "ABL3423SFSD",
+				"Alfajayucan", "agonzalez@hotmail.com", "2221251234",
+				"U123456", "FRSDF1234");
+
+	}
+}
