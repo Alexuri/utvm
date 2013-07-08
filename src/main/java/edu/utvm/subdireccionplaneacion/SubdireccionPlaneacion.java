e0d4f971f3e20a6c46de543aa16b90543160af8f
Ponchoutvm <a20_junio@hotmail.com>
2013-07-08 18:24:39 -0500

    creando clase principal de subdireccionplaneacion

---
+++ src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
@@ -0,0 +1,9 @@
+package edu.utvm.subdireccionplaneacion;
+import edu.utvm.departamentofinanza.deparmentopersonal.JefeArea;
+class SubdireccionPlaneacion{
+  public ProgramacionPresuspuesto programacionpresuspuesto;
+	public ServiciosEscolares serviciosescolares;
+	public PracticasEstadias practicasestadias;
+	public JefeArea jefearea;
+	
+}
