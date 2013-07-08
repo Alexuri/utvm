03152211aff9219713dcdf03dd6efdfb15e01d67
trocheman <jescobedo@utvm.edu.mx>
2013-07-08 16:39:22 -0500

    Update CartaNoAdeudo.java

--- src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/CartaNoAdeudo.java
+++ src/main/java/edu/utvm/subdireccionplaneacion/practicasestadias/CartaNoAdeudo.java
@@ -1,6 +1,6 @@
 package edu.utvm.subdireccionplaneacion.practicasestadias;
-import edu.utvm.subdireccionplaneacion.serviciosescolares.Alumno;
-import edu.utvm.direccioncarrera.Carrera;
+import edu.utvm.subdireccionplaneacion.serviciosescolares.Alumno.*;
+import edu.utvm.direccioncarrera.Carrera.*;
 
 public class CartaNoAdeudo{
 
