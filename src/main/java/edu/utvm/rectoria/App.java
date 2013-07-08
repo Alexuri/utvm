f62c70699e0e4a547b0dbcc242c26faca8c0c791
cesartoii <cesartori_bio@hotmail.com>
2013-07-08 18:26:49 -0500

    Update App.java

--- src/main/java/edu/utvm/rectoria/App.java
+++ src/main/java/edu/utvm/rectoria/App.java
@@ -1,3 +1,17 @@
+package edu.utvm;
+
 public class App{
+
+  public static void main(String[] arg){
+		
+  
+  Rectoria rectoria = null;
+  	rectoria=new Rectoria();
+		
+		DireccionCarrera direccioncarrera = null;
+		DireccionCarrera = direccioncarrera();
+
+		SubdireccionPlaneacion=null;
+		SubdireccionPlaneacion = subdireccionplaneacion();
 
 }
