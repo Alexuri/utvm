b19107a3bde5c08e97f3e8b29d5aaf2ee182c51d
JorgePV <georgefpv30@gmail.com>
2013-07-09 10:36:02 -0500

    corrigiendo errores de la clase

--- src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
+++ src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
@@ -5,7 +5,7 @@ import edu.utvm.subdireccionplaneacion.programacionpresupuesto.*;
 import edu.utvm.subdireccionplaneacion.practicasestadias.*;
 import edu.utvm.subdireccionplaneacion.serviciosescolares.*;
 
-class SubdireccionPlaneacion {
+public class SubdireccionPlaneacion {
 	public ProgramacionPresupuesto programacionpresupuesto;
 	public Escolares serviciosescolares;
 	public PracticasEstadias practicasestadias;
