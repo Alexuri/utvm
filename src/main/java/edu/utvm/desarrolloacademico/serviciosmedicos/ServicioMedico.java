4f8fb9c9a2f8b715e02530f567fb6e7de893924d
Jaime Salathiel Pejay Bautista <sala0211@hotmail.com>
2013-07-09 10:11:43 -0500

    Agregando modificaciones en Constructor

--- src/main/java/edu/utvm/desarrolloacademico/serviciosmedicos/ServicioMedico.java
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosmedicos/ServicioMedico.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 public class ServicioMedico {
 	String salaEspera;
 	String salaConsulta;
-	public List<PersonalMedico> personalMedico;
+	public RegistroImss registroImss;
+	public ArrayList<PersonalMedico> personalMedico;
 
 	public ServicioMedico(String salaEspera, String SalaConsulta,
 			ArrayList<PersonalMedico> personalMedico) {
 		this.personalMedico = new ArrayList<PersonalMedico>();
+		this.registroImss = new RegistroImss(new Alumno(null), null);
+		this.registroImss = new RegistroImss(new Empleado(null, null, null, null, null, null, null, null, null, null), null);
 	}
 
 	public void atencionMedica() {
 
 	}
 }
+
+
 
 
 
