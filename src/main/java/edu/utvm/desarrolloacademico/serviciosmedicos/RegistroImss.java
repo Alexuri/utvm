1b2f7f535d8c1e1ea6c542ee2036ea1dc4bf8445
Jovanywater <jovany_778@hotmail.com>
2013-07-09 10:04:04 -0500

    nueva modificacion RegistroImss
    nueva modificacion RegistroImss

--- src/main/java/edu/utvm/desarrolloacademico/serviciosmedicos/RegistroImss.java
+++ src/main/java/edu/utvm/desarrolloacademico/serviciosmedicos/RegistroImss.java
@@ -8,18 +8,38 @@ import edu.utvm.subdireccionplaneacion.serviciosescolares.*;
 public class RegistroImss {
 	private Alumno alumno;
 	private Empleado empleado;
-	String numeroAfiliacion;
+	public String numeroAfiliacion;
 
 	public RegistroImss(Alumno alumno, String numeroAfiliacion) {
-		this.alumno = new Alumno(numeroAfiliacion);
+		this.setAlumno(alumno);
 		this.numeroAfiliacion = numeroAfiliacion;
+	
 
 	}
 
-	public RegistroImss(Empleado empleado, String numeroAf) {
-		this.empleado = new Empleado(null, null, null, new Date(), null, null,
-				null, null, null, null);
-		this.numeroAfiliacion = numeroAf;
+	public RegistroImss(Empleado empleado, String numeroAfiliacion) {
+		this.setEmpleado(new Empleado(null, null, null, new Date(), null, null,
+				null, null, null, null));
+		this.numeroAfiliacion = numeroAfiliacion;
+	
 	}
+
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(Empleado empleado) {
+		this.empleado = empleado;
+	}
+
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+	
+	
 
 }
