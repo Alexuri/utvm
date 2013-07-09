33dcd9aff390a7105d41aa726a1431cfd2c74f2d
JorgePV <georgefpv30@gmail.com>
2013-07-09 09:21:50 -0500

    realizando correcciones

--- src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
+++ src/main/java/edu/utvm/subdireccionplaneacion/SubdireccionPlaneacion.java
@@ -3,6 +3,7 @@ import edu.utvm.departamentofinanza.departamentopersonal.JefeArea;
 import edu.utvm.subdireccionplaneacion.programacionpresupuesto.PracticasEstadias;
 import edu.utvm.subdireccionplaneacion.programacionpresupuesto.ServiciosEscolares;
 import edu.utvm.subdireccionplaneacion.programacionpresupuesto.PracticasEstadias;
+
 class SubdireccionPlaneacion{
   public ProgramacionPresuspuesto programacionpresuspuesto;
 	public ServiciosEscolares serviciosescolares;
