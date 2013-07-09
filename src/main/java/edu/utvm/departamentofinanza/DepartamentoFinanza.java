60a5b226f9a07697f5d3fa7a57f45a90bb8c8a3c
Israel Cruz Perez <103094@UL-TRON.(none)>
2013-07-09 10:22:42 -0500

    clase principaldepartamento finanzas

---
+++ src/main/java/edu/utvm/departamentofinanza/DepartamentoFinanza.java
@@ -0,0 +1,24 @@
+package edu.utvm.departamentofinanza;
+
+import edu.utvm.departamentofinanza.departamentocontabilidad.DepartamentoContabilidad;
+
+public class DepartamentoFinanza {
+
+	public DepartamentoFinanza() {		
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Construyendo la clase Departamento Finanzas");
+ System.out.println("Construyendo la clase Departamento Contabilidad");
+ System.out.println("Construyendo la clase Departamento Mantenimiento");
+ System.out.println("Construyendo la clase Departamento Personal");
+ System.out.println("Construyendo la clase Departamento Recusrsos");
+	
+		
+		
+	}
+
+}
---
+++ src/main/java/edu/utvm/departamentofinanza/departamentorecursos/DepartamentoRecurso.java
@@ -0,0 +1,13 @@
+package edu.utvm.departamentofinanza.departamentorecursos;
+
+public class DepartamentoRecurso {
+
+	
+	public static void main(String[] args) {
+		Compra compra = new Compra(1, 1111, "laptop");
+		Departamento departamento = new Departamento("xxx", 969696969);
+		System.out.println(compra);
+		System.out.println(departamento);
+	}
+
+}
